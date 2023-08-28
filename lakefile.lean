0382b22a2e6e583236dbabaa911febb4fe5b2c86
adamtopaz <github@adamtopaz.com>
2023-08-28 17:04:32 -0600

    initial commit

---
+++ lakefile.lean
@@ -0,0 +1,14 @@
+import Lake
+open Lake DSL
+
+package «formalization-of-mathematics» {
+  -- add any package configuration options here
+}
+
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git"
+
+@[default_target]
+lean_lib «FormalizationOfMathematics» {
+  -- add any library configuration options here
+}
