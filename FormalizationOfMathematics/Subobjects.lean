b6e422dad5c29674c470823f716ddd8ceb115978
adamtopaz <github@adamtopaz.com>
2023-10-19 12:21:02 -0600

    more on subobjects

--- FormalizationOfMathematics/Subobjects.lean
+++ FormalizationOfMathematics/Subobjects.lean
@@ -1,4 +1,47 @@
 import Mathlib.GroupTheory.Subgroup.Basic
+import Mathlib.Algebra.Ring.Equiv
+
+/-! Some notes-/
+
+-- About morphisms
+-- Classes
+#check FunLike
+#check MonoidHomClass
+#check RingHomClass
+
+-- Morphisms themselves
+#check ∀ {α β}, α → β   
+#check MonoidHom
+#check RingHom
+
+-- About equivalences
+#check EquivLike
+#check MulEquivClass
+#check RingEquivClass
+
+#check ∀ {α β}, α ≃ β 
+#check MulEquiv
+#check RingEquiv
+
+/-!
+
+The `SetLike` class.
+
+-/
+
+-- Thinking about subsets as types via the subtype construction
+
+example {X : Type*} (S : Set X) : Type _ := S
+example {X : Type*} (S : Set X) (x : X) (hx : x ∈ S) : S := 
+  ⟨x, hx⟩ --Subtype.mk x hx
+
+example {X : Type*} (S : Set X) (x : S) : X := x
+example {X : Type*} (S : Set X) (x : S) : ↑x ∈ S := x.property 
+
+example {T X : Type*} [SetLike T X] (S : T) : Type _ := S
+example {T X : Type*} [SetLike T X] (S : T) (x : X) : Prop := x ∈ S
+example {T X : Type*} [SetLike T X] (S : T) (s : S) : X := s
+example {T X : Type*} [SetLike T X] (S : T) (s : S) : ↑s ∈ S := by exact SetLike.coe_mem s 
 
 variable (M : Type*) [Monoid M]
 
@@ -43,22 +86,141 @@ example (H : Submonoid' M) : Type _ := H
 
 example (H : Submonoid' M) (h : H) : M := h
 
-#check Coe
+@[simps]
+instance (H : Submonoid' M) : Mul H where
+  mul a b := ⟨a * b, H.mul_mem _ _ (Submonoid'.property a) (Submonoid'.property b)⟩ 
+
+@[simps]
+instance (H : Submonoid' M) : One H where
+  one := ⟨1, H.one_mem⟩ 
+
 instance (H : Submonoid' M) : Monoid H where
-  mul := fun x y => {
-    val := x * y
-    property := H.mul_mem _ _ (Submonoid'.property x) (Submonoid'.property y)
+  mul_assoc := by
+    intro a b c 
+    ext
+    dsimp
+    apply mul_assoc
+  one_mul := by
+    intro a
+    ext
+    dsimp
+    apply one_mul
+  mul_one := by
+    intro a
+    ext 
+    dsimp
+    apply mul_one
+  npow n m := {
+    val := (m : M)^n
+    property := by
+      induction n with
+      | zero => 
+        simp only [Nat.zero_eq, pow_zero]
+        exact H.one_mem
+      | succ n ih => 
+        change (m : M) ^ (n+1) ∈ H
+        rw [pow_succ]
+        apply H.mul_mem
+        · exact m.property  
+        · assumption } 
+  npow_zero := by
+    intro m
+    ext
+    simp
+  npow_succ := by
+    intro m n
+    ext
+    dsimp
+    apply pow_succ
+
+def Submonoid'.subtype (H : Submonoid' M) : H →* M where
+  toFun m := m
+  map_one' := rfl
+  map_mul' _ _ := rfl
+
+example {A B : Type*} [Monoid A] [Monoid B] (f : A →* B) 
+    (x y : B) (hx : x ∈ Set.range f) (hy : y ∈ Set.range f) :
+    x * y ∈ Set.range f := by
+  dsimp [Set.range] at hx hy ⊢
+  cases hx with
+  | intro x' hx' => 
+    cases hy with
+    | intro y' hy' => 
+      use x' * y'  
+      rw [map_mul, hx', hy']
+
+example {A B : Type*} [Monoid A] [Monoid B] (f : A →* B) 
+    (x y : B) (hx : x ∈ Set.range f) (hy : y ∈ Set.range f) :
+    x * y ∈ Set.range f := by
+  dsimp [Set.range] at hx hy ⊢
+  rcases hx with ⟨x',hx'⟩
+  rcases hy with ⟨y',hy'⟩
+  use x' * y'  
+  rw [map_mul, hx', hy']
+
+example {A B : Type*} [Monoid A] [Monoid B] (f : A →* B) 
+    (x y : B) (hx : x ∈ Set.range f) (hy : y ∈ Set.range f) :
+    x * y ∈ Set.range f := by
+  dsimp [Set.range] at hx hy ⊢
+  obtain ⟨x',hx'⟩ := hx
+  obtain ⟨y',hy'⟩ := hy
+  use x' * y'  
+  rw [map_mul, hx', hy']
+
+example {A B : Type*} [Monoid A] [Monoid B] (f : A →* B) 
+    (x y : B) (hx : x ∈ Set.range f) (hy : y ∈ Set.range f) :
+    x * y ∈ Set.range f := by
+  dsimp [Set.range] at hx hy ⊢
+  obtain ⟨x',rfl⟩ := hx
+  obtain ⟨y',rfl⟩ := hy
+  use x' * y'  
+  rw [map_mul]
+
+example {A B : Type*} [Monoid A] [Monoid B] (f : A →* B) 
+    (x y : B) (hx : x ∈ Set.range f) (hy : y ∈ Set.range f) :
+    x * y ∈ Set.range f := by
+  dsimp [Set.range] at hx hy ⊢
+  rcases hx with ⟨x',rfl⟩
+  rcases hy with ⟨y',rfl⟩
+  use x' * y'  
+  rw [map_mul]
+
+example {A B : Type*} [Monoid A] [Monoid B] (f : A →* B) 
+    (x y : B) (hx : x ∈ Set.range f) (hy : y ∈ Set.range f) :
+    x * y ∈ Set.range f := by
+  dsimp [Set.range] at hx hy ⊢
+  rcases hx with ⟨x',rfl⟩
+  rcases hy with ⟨y',rfl⟩
+  refine ⟨x' * y', ?_⟩
+  simp
+
+def MonoidHom.range' {A B : Type*} [Monoid A] [Monoid B] (f : A →* B) :
+    Submonoid' B where
+  carrier := Set.range f
+  one_mem' := by
+    use 1
+    exact f.map_one
+  mul_mem' := by
+    rintro x y ⟨x',rfl⟩ ⟨y',rfl⟩
+    use x' * y'
+    simp
+
+
+/-! The order structure on subobjects -/
+
+instance : InfSet (Submonoid' M) where
+  sInf S := {
+    carrier := sInf (Submonoid'.carrier '' S)
+    one_mem' := sorry
+    mul_mem' := sorry
   }
-  mul_assoc := sorry 
-  one := {
-    val := 1
-    property := H.one_mem
-  }
-  one_mul := sorry
-  mul_one := sorry
-  npow n h := {
-    val := (h : M)^n
-    property := sorry
-  }
-  npow_zero := sorry
-  npow_succ := sorry
+
+instance : CompleteLattice (Submonoid' M) := 
+  completeLatticeOfInf (Submonoid' M) <| by
+    intro S
+    dsimp [IsGLB, IsGreatest]
+    refine ⟨?_, ?_⟩ 
+    · dsimp [lowerBounds]
+      intro A hA x hx
+      sorry
+    · sorry 
